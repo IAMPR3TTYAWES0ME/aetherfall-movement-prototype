c174deab376a7553fb258aaa9c1c467f8e6f4412
IAMPR3TTYAWES0ME <IAMPR3TTYAWES0ME@outlook.com>
2026-01-05 12:47:58 -0600

    Completely made the walk and the directioning

--- scripts/movement_manager.gd
+++ scripts/movement_manager.gd
@@ -1,11 +1,11 @@
 extends CharacterBody3D
-@onready var camera_3d: Camera3D = $"Erika Archer/CameraPivot/SpringArm3D/Camera3D"
-@onready var camera_pivot: Node3D = $"Erika Archer/CameraPivot"
-@onready var spring_arm_3d: SpringArm3D = $"Erika Archer/CameraPivot/SpringArm3D"
+@onready var camera_3d: Camera3D = $CameraPivot/SpringArm3D/Camera3D
+@onready var camera_pivot: Node3D = $CameraPivot
+@onready var spring_arm_3d: SpringArm3D = $CameraPivot/SpringArm3D
 @onready var character: Node3D = $"Erika Archer"
 @onready var animation_handler: AnimationPlayer = $"Erika Archer/animation_handler"
 @onready var anim_tree: AnimationTree = $"Erika Archer/AnimationTree"
-
+@onready var player: CharacterBody3D = $"."
 
 
 const SPEED = 5.0
@@ -13,9 +13,9 @@ const JUMP_VELOCITY = 4.5
 var mouse_sensitivity := 0.003
 var pitch := 0.0
 var plrRotation := 0.0
-var is_walking := false
-var is_jumping := false
 var playback: AnimationNodeStateMachinePlayback
+var turn_speed := 10.0
+
 
 func _ready() -> void:
 	anim_tree.active = true
@@ -29,8 +29,8 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		is_jumping = true
+		#velocity.y = JUMP_VELOCITY
+		pass
 	
 	
 	# Handle zoom.
@@ -41,18 +41,36 @@ func _physics_process(delta: float) -> void:
 	var lock = clamp(spring_arm_3d.spring_length, 0.5, 5.0)
 	spring_arm_3d.spring_length = lock
 	
-	# Get the input direction and handle the movement/deceleration.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-		is_walking = true
+
+		# Camera axes in world space
+	var cam_basis := camera_pivot.global_transform.basis
+	var cam_right := -cam_basis.x
+	var cam_forward := -cam_basis.z
+
+	# Flatten (ignore pitch) BEFORE using them
+	cam_right.y = 0
+	cam_forward.y = 0
+	cam_right = cam_right.normalized()
+	cam_forward = cam_forward.normalized()
+
+	# Build camera-relative movement direction (world space)
+	var move_dir := cam_right * input_dir.x + cam_forward * input_dir.y
+
+	if move_dir.length() > 0.001:
+		move_dir = move_dir.normalized()
+
+		velocity.x = move_dir.x * SPEED
+		velocity.z = move_dir.z * SPEED
+
+		# Face the direction you're actually moving
+		var target_yaw := atan2(-move_dir.x, -move_dir.z)
+		character.rotation.y = lerp_angle(character.rotation.y, target_yaw, turn_speed * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-		is_walking = false
+		velocity.x = move_toward(velocity.x, 0.0, SPEED)
+		velocity.z = move_toward(velocity.z, 0.0, SPEED)
+
+	# animation handler
 	_update_animation(input_dir)
 
 	
@@ -62,6 +80,8 @@ func _physics_process(delta: float) -> void:
 		camera_pivot.rotation.y -= cam.x * controller_sens * delta
 		pitch = clamp(pitch - cam.y * controller_sens * delta, deg_to_rad(-60), deg_to_rad(60))
 		spring_arm_3d.rotation.x = pitch
+	if Input.is_action_pressed("Zoom_for_xbox"):
+		spring_arm_3d.spring_length = 0.5
 
 	move_and_slide()
 	
@@ -69,8 +89,12 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion && Input.is_action_pressed("Orbit_Unlock"):
 		var _mouse_x = event.relative.x
 		var _mouse_y = event.relative.y
+		
+		#left and right
 		camera_pivot.rotation.y -= _mouse_x * mouse_sensitivity
+		#up and down
 		spring_arm_3d.rotation.x -= _mouse_y * mouse_sensitivity
+		
 		pitch -= _mouse_y * mouse_sensitivity
 		pitch = clamp(pitch, deg_to_rad(-60), deg_to_rad(60))
 		spring_arm_3d.rotation.x = pitch
@@ -80,7 +104,7 @@ var last_target: StringName = &""
 func _update_animation(input_dir: Vector2) -> void:
 	var target: StringName
 
-	if not is_on_floor():
+	if Input.is_action_just_pressed("Jump"): #not is_on_floor():
 		target = &"jump"
 	elif input_dir.length() > 0.1:
 		target = &"walk"
