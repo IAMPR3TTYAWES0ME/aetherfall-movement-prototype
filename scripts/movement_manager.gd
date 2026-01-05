a8cab91ec42ed44a42cc64fd661629692ba31763
IAMPR3TTYAWES0ME <IAMPR3TTYAWES0ME@outlook.com>
2026-01-05 16:14:28 -0600

    Gonna practice raycasts, Ill get back to this later

--- scripts/movement_manager.gd
+++ scripts/movement_manager.gd
@@ -8,35 +8,44 @@ extends CharacterBody3D
 @onready var player: CharacterBody3D = $"."
 
 
-const SPEED = 5.0
+var SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var mouse_sensitivity := 0.003
 var pitch := 0.0
 var plrRotation := 0.0
 var playback: AnimationNodeStateMachinePlayback
 var turn_speed := 10.0
-
+var running := false
+var zoom = 0
+var death_height = -10
+var con_scroll_enabled := false
 
 func _ready() -> void:
 	anim_tree.active = true
 	playback = anim_tree.get("parameters/playback") as AnimationNodeStateMachinePlayback
 
 func _physics_process(delta: float) -> void:
-	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-		pass
+		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_pressed("sprint"):
+		running = true
+	elif Input.is_action_just_released("sprint"):
+		running = false
 	
-	
+	if Input.is_action_just_pressed("console_sprint"):
+		running = true
 	# Handle zoom.
 	if Input.is_action_just_pressed("scroll_back"):
+		con_scroll_enabled = false
 		spring_arm_3d.spring_length += 0.1
 	if Input.is_action_just_pressed("scroll_forward"):
+		con_scroll_enabled = false
 		spring_arm_3d.spring_length -= 0.1
 	var lock = clamp(spring_arm_3d.spring_length, 0.5, 5.0)
 	spring_arm_3d.spring_length = lock
@@ -80,9 +89,19 @@ func _physics_process(delta: float) -> void:
 		camera_pivot.rotation.y -= cam.x * controller_sens * delta
 		pitch = clamp(pitch - cam.y * controller_sens * delta, deg_to_rad(-60), deg_to_rad(60))
 		spring_arm_3d.rotation.x = pitch
-	if Input.is_action_pressed("Zoom_for_xbox"):
-		spring_arm_3d.spring_length = 0.5
-
+	if Input.is_action_just_pressed("Zoom_for_console"):
+		zoom += 1
+		con_scroll_enabled = true #Later add a setting in the menu to turn it off
+	if zoom == 0  && con_scroll_enabled:
+		spring_arm_3d.spring_length = 1.0
+	elif zoom == 1  && con_scroll_enabled:
+		spring_arm_3d.spring_length = 2.5
+	elif zoom == 2  && con_scroll_enabled:
+		spring_arm_3d.spring_length = 5.0
+	elif zoom > 2  && con_scroll_enabled:
+		zoom = 0
+		
+	_death(death_height)
 	move_and_slide()
 	
 func _input(event: InputEvent) -> void:
@@ -106,12 +125,23 @@ func _update_animation(input_dir: Vector2) -> void:
 
 	if Input.is_action_just_pressed("Jump"): #not is_on_floor():
 		target = &"jump"
-	elif input_dir.length() > 0.1:
+	elif input_dir.length() > 0.1 && running == true:
+		target = &"run"
+		SPEED = 7.5
+	elif input_dir.length() > 0.1 && running == false:
 		target = &"walk"
+		SPEED = 5.0
 	else:
 		target = &"idle"
+	
+	if input_dir.length() < 0.1:
+		running = false
 
 	# Only send travel if the target changed since last frame
 	if target != last_target:
 		last_target = target
 		playback.travel(target)
+		
+func _death(death: float):
+		if player.position.y <= death:
+			get_tree().reload_current_scene()
